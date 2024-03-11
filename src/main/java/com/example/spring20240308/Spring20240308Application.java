54c7c21aea044a845be1e13096fdd40e84136ccb
mjkim <mjkim@couptech.co.kr>
2024-03-11 10:32:49 +0900

    first commit

---
+++ src/main/java/com/example/spring20240308/Spring20240308Application.java
@@ -0,0 +1,13 @@
+package com.example.spring20240308;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20240308Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20240308Application.class, args);
+    }
+
+}
